ab02206e22aba10c29f79c1349498e0e57b204ac
bronson.duhart@gmail.com <mrduhart>
2023-12-06 20:39:37 -0600

    format with black

--- test_assert_examples.py
+++ test_assert_examples.py
@@ -11,5 +11,7 @@ def test_reversed():
 
 def test_some_primes():
     assert 37 in {
-        num for num in range(2, 50) if not any(num % div == 0 for div in range(2, num))
+        num
+        for num in range(2, 50)
+        if not any(num % div == 0 for div in range(2, num))
     }
