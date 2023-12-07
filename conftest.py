223a392ce78f0dee2d9021b49896bb931a45a068
bronson.duhart@gmail.com <mrduhart>
2023-12-07 15:23:43 -0600

    try pytest-cov

--- conftest.py
+++ conftest.py
@@ -7,6 +7,6 @@ import requests
 @pytest.fixture(autouse=True)
 def disable_network_calls(monkeypatch):
     def stunted_get():
-        raise RuntimeError("Network access not allowed during testing!")
+        raise RuntimeError("Network access not allowed during testing!") 
 
     monkeypatch.setattr(requests, "get", lambda *args, **kwargs: stunted_get())
