dc0d6c62ba2720dc96d77f0764263791a7bcc5a9
bronson.duhart@gmail.com <mrduhart>
2023-12-06 22:15:11 -0600

    implement xfail

--- test_with_pytest.py
+++ test_with_pytest.py
@@ -1,9 +1,11 @@
 # test_with_pytest.py
+import pytest
 
 
 def test_always_passes():
     assert True
 
 
+@pytest.mark.xfail(True, reason="Always fails")
 def test_always_fails():
     assert False
