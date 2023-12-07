f4f380408217c7a6ea86fd66374732367e047a4e
bronson.duhart@gmail.com <mrduhart>
2023-12-06 22:15:28 -0600

    split palindrome test

--- test_palindrome.py
+++ test_palindrome.py
@@ -4,16 +4,25 @@ from palindrome import is_palindrome
 
 
 @pytest.mark.parametrize(
-    "maybe_palindrome, expected_result",
+    "palindrome",
     [
-        ("", True),
-        ("a", True),
-        ("Bob", True),
-        ("Never odd or even", True),
-        ("Do geese see God?", True),
-        ("abc", False),
-        ("abab", False),
+        "",
+        "a",
+        "Bob",
+        "Never odd or even",
+        "Do geese see God?",
     ],
 )
-def test_is_palindrome(maybe_palindrome, expected_result):
-    assert is_palindrome(maybe_palindrome) == expected_result
+def test_is_palindrome(palindrome):
+    assert is_palindrome(palindrome)
+
+
+@pytest.mark.parametrize(
+    "non_palindrome",
+    [
+        "abc",
+        "abab",
+    ],
+)
+def test_is_not_palindrome(non_palindrome):
+    assert not is_palindrome(non_palindrome)
